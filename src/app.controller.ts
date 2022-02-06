0c6b5f44632a531592c4609263c19ee8f2df7658
BD Dinh <bd.dinh@gmail.com>
2022-02-06 12:52:42 -0500

    Correction du endPoint pour charger les données

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+   // http://localhost:3000/loadData
   @Get("/loadData")
   loadData(): Promise<String> {
     return this.appService.loadAllData();
